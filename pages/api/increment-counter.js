fb3f09e8211668b0ccaea9bcb9ec03290638d17e
ankush <webmaster.ankush@gmail.com>
2024-07-05 23:35:07 +0530

    Update increment-counter.js

--- pages/api/increment-counter.js
+++ pages/api/increment-counter.js
@@ -1,1 +1,11 @@
+let visitorCount = 0;
 
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    visitorCount += 1;
+    res.status(200).json({ count: visitorCount });
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
