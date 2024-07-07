cb57fdda8579b021ee5826d07397745f80d359fa
ankush <webmaster.ankush@gmail.com>
2024-07-07 12:30:07 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -107,7 +107,7 @@ export default function Home() {
               )}
             </Segment>
             {/* Adsterra Ad */}
-            <AdsterraAd adScriptSrc="//controlaffliction.com/7f4afa6163e1c4f538d5ed0af889234b/invoke.js" adContainerId="ad-container-1" />
+            <AdsterraAd adScriptSrc="//controlaffliction.com/84/f9/d8/84f9d89ff5bccd06e0d241d0a278b798.js" adContainerId="ad-container-1" />
           </Grid.Column>
           <Grid.Column></Grid.Column>
         </Grid.Row>
@@ -122,8 +122,8 @@ export default function Home() {
             <a href="https://cheapgeeky.com" target="_blank" rel="noreferrer"><Icon name='external' /> Visit CheapGeeky</a>
             <p>Made with ♥️ by Ankush.</p>
             {/* Adsterra Ads */}
-            <AdsterraAd adScriptSrc="//controlaffliction.com/7935fdc40a369b1b8e7fcfd0f9435185/invoke.js" adContainerId="ad-container-2" />
-            <AdsterraAd adScriptSrc="//controlaffliction.com/84/f9/d8/84f9d89ff5bccd06e0d241d0a278b798.js" adContainerId="ad-container-3" />
+            <AdsterraAd adScriptSrc="//controlaffliction.com/7f4afa6163e1c4f538d5ed0af889234b/invoke.js" adContainerId="ad-container-2" />
+            <AdsterraAd adScriptSrc="//controlaffliction.com/7935fdc40a369b1b8e7fcfd0f9435185/invoke.js" adContainerId="ad-container-3" />
             <AdsterraAd adScriptSrc="//controlaffliction.com/44ae6eacdda63238ece6e65059c59ec8/invoke.js" adContainerId="ad-container-4" />
           </Grid.Column>
           <Grid.Column></Grid.Column>
