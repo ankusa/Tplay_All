7f7e4c7311e8e3bb92190de3172a7d9467ad51a2
ankush <webmaster.ankush@gmail.com>
2024-07-07 14:38:19 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,11 @@
 import Head from 'next/head';
-import Script from 'next/script';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Grid, Message, Segment, Icon, Image } from 'semantic-ui-react';
+import { Container, Grid, Header, Icon, Image, Menu, Segment, Message } from 'semantic-ui-react';
 
 export default function Home() {
   const [shortUrl, setShortUrl] = useState("");
   const [err, setErr] = useState("");
-  const [visitorCount, setVisitorCount] = useState(0);
-  const [country, setCountry] = useState("");
 
   useEffect(() => {
     const url = `${window.location.origin.replace('localhost', '127.0.0.1')}/api/getM3u?sid=tplay_A&id=1028268934&sname=tataP&tkn=cheapgeeky.com`;
@@ -19,9 +16,6 @@ export default function Home() {
         console.error('Error generating short URL:', error);
         setErr('Error generating short URL. Please try refreshing the page.');
       });
-
-    getVisitorInfo();
-    incrementVisitorCounter();
   }, []);
 
   async function shortenUrl(longUrl) {
@@ -50,97 +44,86 @@ export default function Home() {
     }
   }
 
-  async function getVisitorInfo() {
-    try {
-      const response = await axios.get('https://ipinfo.io?token=e0f28ce078e7c9');
-      setCountry(response.data.country);
-    } catch (error) {
-      console.error('Error fetching visitor info:', error);
-    }
-  }
-
-  async function incrementVisitorCounter() {
-    try {
-      const response = await axios.post('/api/increment-counter');
-      console.log('Increment Counter Response:', response.data);
-      setVisitorCount(response.data.count);
-    } catch (error) {
-      console.error('Error incrementing visitor counter:', error);
-    }
-  }
-
   return (
     <div>
       <Head>
         <title>TATA PLAY COPY PASTE M3U</title>
         <meta name="description" content="Easiest way to generate a Tata Play IPTV (m3u) playlist." />
       </Head>
-      <Script
-        src="//controlaffliction.com/dd/2e/d4/dd2ed46f3a77fc9150b15baae76d2cdb.js"
-        strategy="afterInteractive"
-      />
-      <Grid columns='equal' padded centered>
-        <Grid.Row>
-          <Grid.Column></Grid.Column>
-          <Grid.Column computer={8} tablet={12} mobile={16}>
-            <Segment>
-              <Image src='https://upload.wikimedia.org/wikipedia/commons/2/29/Tata_Play_2022_logo.svg' centered size='big' alt='Tata Play' />
-              <Message>
-                <Message.Header><Icon name='linkify' /> M3U Short URL:</Message.Header>
-                {shortUrl ? (
+      <Menu fixed='top' inverted>
+        <Container>
+          <Menu.Item header>
+            <Image
+              size='mini'
+              src='/logo.png'
+              style={{ marginRight: '1.5em' }}
+            />
+            Tata Play
+          </Menu.Item>
+          <Menu.Item as='a' href="https://cheapgeeky.com" target="_blank" rel="noreferrer">
+            Visit CheapGeeky
+          </Menu.Item>
+        </Container>
+      </Menu>
+      <Container style={{ marginTop: '7em' }}>
+        <Grid columns='equal' padded centered>
+          <Grid.Row>
+            <Grid.Column></Grid.Column>
+            <Grid.Column computer={8} tablet={12} mobile={16}>
+              <Segment>
+                <Image src='https://upload.wikimedia.org/wikipedia/commons/2/29/Tata_Play_2022_logo.svg' centered size='big' alt='Tata Play' />
+                <Message>
+                  <Message.Header><Icon name='linkify' /> M3U Short URL:</Message.Header>
+                  {shortUrl ? (
+                    <p>
+                      <a href={shortUrl} target="_blank" rel="noreferrer">{shortUrl}</a> 🎉
+                    </p>
+                  ) : (
+                    <p>Generating short URL... ⏳</p>
+                  )}
                   <p>
-                    <a href={shortUrl} target="_blank" rel="noreferrer">{shortUrl}</a> 🎉
+                    Use the M3U URL in the OTT Navigator or Tivimate app for all channels.
                   </p>
-                ) : (
-                  <p>Generating short URL... ⏳</p>
-                )}
-                <p>
-                  Use the M3U URL in the OTT Navigator or Tivimate app for all channels.
-                </p>
-                <p>
-                  Set data reload to 10 minutes and enjoy uninterrupted viewing!
-                </p>
-                <p>The generated M3U URL is permanent and does not need to be refreshed every 24 hours. Enjoy!</p>
-                <p><strong>IMPORTANT:</strong> If you encounter an error with the generated URL, it might be due to an API issue. Simply wait for the API issue to be resolved.</p>
-              </Message>
-              {err && (
-                <Message negative>
-                  <Message.Header><Icon name='exclamation circle' /> Error</Message.Header>
-                  <p>{err}</p>
+                  <p>
+                    Set data reload to 10 minutes and enjoy uninterrupted viewing!
+                  </p>
+                  <p>The generated M3U URL is permanent and does not need to be refreshed every 24 hours. Enjoy!</p>
+                  <p><strong>IMPORTANT:</strong> If you encounter an error with the generated URL, it might be due to an API issue. Simply wait for the API issue to be resolved.</p>
                 </Message>
-              )}
-            </Segment>
-          </Grid.Column>
-          <Grid.Column></Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column></Grid.Column>
-          <Grid.Column textAlign='center' computer={8} tablet={12} mobile={16}>
-            <Message>
-              <Message.Header><Icon name='world' /> Visitor Information</Message.Header>
-              <p>Visitor Count: {visitorCount}</p>
-              <p>Country: {country}</p>
-            </Message>
-            <a href="https://cheapgeeky.com" target="_blank" rel="noreferrer"><Icon name='external' /> Visit CheapGeeky</a>
-            <p>Made with ♥️ by Ankush.</p>
-            <div style={{ margin: '20px 0' }}>
-              <Script
-                src="//controlaffliction.com/7935fdc40a369b1b8e7fcfd0f9435185/invoke.js"
-                strategy="afterInteractive"
-              />
-              <div id="container-7935fdc40a369b1b8e7fcfd0f9435185"></div>
-            </div>
-            <div style={{ margin: '20px 0' }}>
-              <Script
-                src="https://controlaffliction.com/e2butenyvy?key=399aebd799b150e0683df07e9b033ae3"
-                strategy="afterInteractive"
-                async
-              />
-            </div>
-          </Grid.Column>
-          <Grid.Column></Grid.Column>
-        </Grid.Row>
-      </Grid>
+                {err && (
+                  <Message negative>
+                    <Message.Header><Icon name='exclamation circle' /> Error</Message.Header>
+                    <p>{err}</p>
+                  </Message>
+                )}
+              </Segment>
+            </Grid.Column>
+            <Grid.Column></Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Segment inverted vertical style={{ padding: '5em 0em' }}>
+          <Container>
+            <Grid divided inverted stackable>
+              <Grid.Row>
+                <Grid.Column width={7}>
+                  <Header as='h4' inverted>
+                    Tata Play M3U Generator
+                  </Header>
+                  <p>Made with ♥️ by Ankush.</p>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                  <Header as='h4' inverted>
+                    Links
+                  </Header>
+                  <Menu.Item as='a' href="https://cheapgeeky.com" target="_blank" rel="noreferrer">
+                    Visit CheapGeeky
+                  </Menu.Item>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Segment>
+      </Container>
     </div>
   );
-                  }
+                    }
